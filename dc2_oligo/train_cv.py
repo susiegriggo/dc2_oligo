a5b974dbdb1c0d1d35cde4d382c294971d437a8c
grig0076 <susie.grigson@flinders.edu.au>
2024-12-06 13:48:09 +1030

    swap to monomer prediction

--- dc2_oligo/train_cv.py
+++ dc2_oligo/train_cv.py
@@ -14,13 +14,14 @@ from utils import get_af2_emb
 
 
 def train(
-    af2_embeddings_dir: str = '../calc',c=10, balanced=0, dual=1,
+    training_data, af2_embeddings_dir: str = '../calc',c=10, balanced=0, dual=1,
           ensemble_size=1, use_pairwise=True, use_scaler=True, output_dir: str = None
           ) -> tuple:
     """
     Train an ensemble of logistic regression models.
 
     Parameters:
+        training_data (str): Path to training data.
         c (int, optional): Regularization parameter. Defaults to 10.
         balanced (int, optional): Whether to balance class weights. Defaults to 0.
         dual (int, optional): Whether to use dual formulation. Defaults to 1.
@@ -32,7 +33,7 @@ def train(
         dict: Dictionary containing training results, trained models, and DataFrame with results.
     """
 
-    df = pd.read_csv("../tests/set5_homooligomers.csv", sep="\t")
+    df = pd.read_csv(training_data, sep="\t")
     le = LabelEncoder()
     df['y'] = le.fit_transform(df['chains'])\
 
@@ -78,6 +79,73 @@ def train(
 
     return results_, model, df
 
+
+def train_monomer(
+    training_data, af2_embeddings_dir: str = '../calc',c=10, balanced=0, dual=1,
+          ensemble_size=1, use_pairwise=True, use_scaler=True, output_dir: str = None
+          ) -> tuple:
+    """
+    Train an ensemble of logistic regression models. Same as above but only predicts whether a protein in a homomomer or monomer 
+
+    Parameters:
+        training_data (str): Path to training data.
+        c (int, optional): Regularization parameter. Defaults to 10.
+        balanced (int, optional): Whether to balance class weights. Defaults to 0.
+        dual (int, optional): Whether to use dual formulation. Defaults to 1.
+        ensemble_size (int, optional): Number of ensemble models. Defaults to 1.
+        use_pairwise (bool, optional): Whether to use pairwise embeddings. Defaults to True.
+        use_scaler (bool, optional): Whether to use data scaling. Defaults to True.
+
+    Returns:
+        dict: Dictionary containing training results, trained models, and DataFrame with results.
+    """
+
+    df = pd.read_csv(training_data, sep="\t")
+    df['state'] = np.min([1 if i>1 else 0 for i in df['chains']])
+    le = LabelEncoder()
+    df['y'] = le.fit_transform(df['state'])\
+
+    results = np.zeros((ensemble_size, 5, len(df), 3))
+    model = {}
+    for j in range(0, ensemble_size):
+        for i in range(0, 5): # 5 since we have 5 AF2 models
+
+            X = np.asarray([get_af2_emb(af2_embeddings_dir,id_=id_, model_id=i, use_pairwise=use_pairwise) for id_ in df.index])
+            y = df['y'].values
+
+            cv = KFold(n_splits=5, shuffle=True)
+
+            for k, (tr_idx, te_idx) in enumerate(cv.split(X, y)):
+
+                X_tr, X_te = X[tr_idx], X[te_idx]
+                y_tr, y_te = y[tr_idx], y[te_idx]
+
+                if use_scaler == 1:
+                    sc = StandardScaler()
+                    X_tr = sc.fit_transform(X_tr)
+                    X_te = sc.transform(X_te)
+                    model[f"scaler_{j}_{i}_{k}"] = sc
+                clf = LogisticRegression(C=c, max_iter=1000, solver='liblinear',
+                                        dual=False if dual == 0 else True,
+                                        class_weight='balanced' if balanced == 1 else None)
+                clf.fit(X_tr, y_tr)
+                results[j, i, te_idx, :] = clf.predict_proba(X_te)
+                model[f"clf_{j}_{i}_{k}"] = clf
+    y_pred_bin = results.mean(axis=0).mean(axis=0).argmax(axis=1)
+    results_ = {}
+    results_["accuracy"] = accuracy_score(y, y_pred_bin)
+    results_["f1"] = f1_score(y, y_pred_bin, average='macro')
+    df["y_pred"] = y_pred_bin
+    df["prob_monomer"] = results.mean(axis=0).mean(axis=0)[:, 0]
+    df["prob_homomer"] = results.mean(axis=0).mean(axis=0)[:, 1]
+    if output_dir:
+        joblib.dump(model, f"{output_dir}/model.p")
+        df.to_csv(f'{output_dir}/results.csv')
+
+    print(results_)
+
+    return results_, model, df
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Train script')
@@ -89,8 +157,9 @@ if __name__ == "__main__":
     parser.add_argument('--use_scaler', type=int, default=1)
     parser.add_argument('--use_pairwise', type=int, default=1)
     parser.add_argument('--output_dir', type=str, default=None)
+    parser.add_argument('--training_data', type=int, default=None)
 
     args = parser.parse_args()
 
-    results, model, df = train(args.af2_embeddings_dir, args.C, args.balanced, args.dual,
+    results, model, df = train_monomer(args.training_data,args.af2_embeddings_dir, args.C, args.balanced, args.dual,
                                args.ensemble_size, args.use_pairwise, args.use_scaler, args.output_dir)
